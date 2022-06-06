ef7676d6d0677c90e79700a80244c681f5e1c4cb
Sohel Akhtar <sohelakhtar23@gmail.com>
2022-06-06 07:38:59 +0545

    Completed chat msg classification

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request
 import model
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
+socketio = SocketIO(app)
 
 @app.route('/')
 def index():
@@ -21,8 +25,25 @@ def predict():
     return render_template("index.html", msg = predict)
 
 
+@app.route('/session', methods=["POST"])
+def session():
+    if request.method == "POST":
+        user_name = request.form["username"]
+        print(user_name)
+    return render_template('session.html', user = user_name)
 
+def messageReceived(methods=['GET', 'POST']):
+    print('message was received!!!')
 
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    print('received my event: ' + str(json))
+    msg_text = str(json["message"])
+    json["category"] = str(model.email_prediction(msg_text))
+    print(str(json))
+    socketio.emit('my response', json, callback=messageReceived)
+    
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    socketio.run(app, debug=True)
+    # app.run(debug=True)
--- model.py
+++ model.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
+
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import recall_score, precision_score, f1_score
 
